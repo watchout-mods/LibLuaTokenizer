8df1afd734fd4e737141adbe71f620a8a72af601
Watchout <watchout@curseforge.com>
2013-06-26 14:11:55 +0200

    Inconsequential testing and profiling tweaks

--- test/Test.php
+++ test/Test.php
@@ -3,6 +3,7 @@ error_reporting(E_ALL);
 
 #system("lua Main.lua < Main.lua | tee");
 system("lua Main.lua < Test.lua | tee");
+#system("lua Main.lua < ..\\LuaTokenizer-1.0.lua");
 
 #echo file_get_contents("Test.html");
 
