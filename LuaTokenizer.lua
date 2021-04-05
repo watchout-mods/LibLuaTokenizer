d320f80ecbf6ccf2f8e1d30f9542d55b61523280
watchout-mods <76425756+watchout-mods@users.noreply.github.com>
2021-04-05 21:55:18 +0200

    Fix tokenizer bug for multiline strings and comments

--- LuaTokenizer.lua
+++ LuaTokenizer.lua
@@ -78,54 +78,57 @@ local function push_char(token, newstate, more)
 	newstate = newstate or "START";
 	return function(states, stack, str, ret, cb, pos, ln)
 		local s = stack[1];
-		stack[2], stack[1] = nil, stack[2];
+		-- stack[2], stack[1] = nil, stack[2];
 --print(("PUSH CHAR\t%q\t%q LEN	%d	POS	%d"):format(token or s, s, #stack, pos))
-		ret[#ret+1] = cb(token or s, s, ln, ln, pos-#s, pos-1, more)
-		return states[newstate], stack, pos, ln;
+		ret[#ret + 1] = cb(token or s, s, ln, ln, pos - #s, pos - 1, more)
+		return states[newstate], {}, pos, ln;
 	end
 end
+
 local function push_token(token, newstate, more)
 	newstate = newstate or "START";
 	return function(states, stack, str, ret, cb, pos, ln)
-		local s = tconcat(stack, "", 1, #stack-1);
-		stack = {stack[#stack]};
+		-- local s = tconcat(stack, "", 1, #stack - 1);
+		local s = tconcat(stack, "", 1, #stack - 1);
 --print(("PUSH TOKEN\t%q\t%q"):format(token or s, s))
-		ret[#ret+1] = cb(token or s, s, ln, ln, pos-#s, pos-1, more)
-		return states[newstate], stack, pos, ln;
+		ret[#ret + 1] = cb(token or s, s, ln, ln, pos - #s, pos - 1, more)
+		return states[newstate], {}, pos, ln;
 	end
 end
+
 local function push_id(newstate)
 	newstate = newstate or "START";
 	return function(states, stack, str, ret, cb, pos, ln)
-		local s, token = tconcat(stack, "", 1, #stack-1), "ID";
-		stack = {stack[#stack]};
-		if keywords[s] then token = "KEYWORD" end
+		local s = tconcat(stack, "", 1, #stack - 1);
+		local token = keywords[s] and "KEYWORD" or "ID";
 --print(("PUSH ID\t%q\t%q"):format(token or s, s))
-		ret[#ret+1] = cb(token or s, s, ln, ln, pos-#s, pos-1)
-		return states[newstate], stack, pos, ln;
+		ret[#ret + 1] = cb(token or s, s, ln, ln, pos - #s, pos - 1)
+		return states[newstate], {}, pos, ln;
 	end
 end
+
 local function add_line(x)
 	if type(x) == "function" then
-		return function(a,b,c,d,e,f,l) return x(a,b,c,d,e,f,l+1) end
+		return function(a, b, c, d, e, f, l) return x(a, b, c, d, e, f, l + 1) end
 	else
 		x = x or "START";
-		return function(a,b,c,d,e,p,l) return states[x], b, p, l+1 end
+		return function(a, b, c, d, e, p, l) return states[x], b, p, l + 1 end
 	end
 end
+
 local function consume_block(token, newstate, stackpos)
 	newstate = newstate or "START";
+	stackpos = stackpos or 2;
 	return function(states, stack, str, ret, cb, pos, ln)
-		local open = tconcat(stack, "", stackpos or 2, #stack-2);
-		local find = ("^(.-%%]%s%%])(.?)"):format(open);
+		local open = stackpos == 0 and "" or tconcat(stack, "", stackpos, #stack - 2);
+		local find = "^(.-%]" .. open .. "%])(.?)";
 		local pos1, pos2, blk, nxt = str:find(find, pos);
 --print("CONSUME", find, pos, pos1, pos2, blk)
 		if pos1 then
 			stack[#stack] = blk; -- stack[#stack] => was first ch of block from statemachine
 			local s = tconcat(stack);
-			ret[#ret+1] = cb(token or s, s, ln, ln, pos-#open-2, pos2-1, more)
-			stack = {nxt};
-			return states[newstate], stack, pos2, ln;
+			ret[#ret + 1] = cb(token or s, s, ln, ln, pos - #open - 2, pos2 - 1, more);
+			return states[newstate], {}, pos2, ln;
 		else
 			return states[ERROR], stack, pos, ln;
 		end
@@ -161,14 +164,16 @@ states = {
 		["~"] = {
 			[F] = push_token("ERROR"), -- if left out, next char would be in error token too
 			["="] = {[F] = push_token(nil)},},
-		[list("^","/","*","+","%","#",",","]","(",")","{","}",":","\t",";")]
-			= {[F] = push_char(nil)},
-		[" "] = {[" "]="BLANK", [F]=push_char("WHITESPACE")},
+		[list("^","/","*","+","%","#",",","]","(",")","{","}",":",";")] = {
+			[F] = push_char(nil)},
+		[list(" ", "\t")] = {
+			[list(" ", "\t")] = "BLANK",
+			[F] = push_char("WHITESPACE")},
 		["-"] = {
 			["-"] = "COMMENT",
 			[F] = push_char(nil)}},
 	BLANK = {
-		[" "] = "BLANK",
+		[list(" ", "\t")] = "BLANK",
 		[F] = push_token("WHITESPACE")},
 	STRINGA = {
 		["'"] = {[F] = push_token("STRING")},
@@ -198,37 +203,40 @@ states = {
 		["\n"] = {[F] = push_token("ERROR")},
 		[ANY] = "STRINGB"},
 	NUMBER = {
-		[range("1","9")] = "NUMBER2",
+		[range("1", "9")] = "NUMBER2",
 		["."] = "NUMBER2",
 		["x"] = "HEXNUM",
-		[list("e","E")] = "NUMBER3",
+		[list("e", "E")] = "NUMBER3",
 		[F] = push_token("NUMBER")},
-	NUMBER2 = {[range("0","9")] = "NUMBER2", [list("e","E")] = "NUMBER3", [F] = push_token("NUMBER")},
-	NUMBER3 = {[range("0","9")] = "NUMBER5", ["-"] = "NUMBER4"},
-	NUMBER4 = {[range("0","9")] = "NUMBER5" },
-	NUMBER5 = {[range("0","9")] = "NUMBER5", [F] = push_token("NUMBER")},
+	NUMBER2 = {[range("0", "9")] = "NUMBER2", [list("e", "E")] = "NUMBER3", [F] = push_token("NUMBER")},
+	NUMBER3 = {[range("0", "9")] = "NUMBER5", ["-"] = "NUMBER4"},
+	NUMBER4 = {[range("0", "9")] = "NUMBER5"},
+	NUMBER5 = {[range("0", "9")] = "NUMBER5", [F] = push_token("NUMBER")},
 	HEXNUM = {
-		[range("0","9")] = "HEXNUM",
-		[range("a","f")] = "HEXNUM",
-		[range("A","F")] = "HEXNUM",
+		[range("0", "9")] = "HEXNUM",
+		[range("a", "f")] = "HEXNUM",
+		[range("A", "F")] = "HEXNUM",
 		[F] = push_token("HEXNUM")},
 	ID = {
 		[class_id()] = "ID",
 		[F] = push_id()},
-	BRACKET  = {["["] = {[F]=consume_block("STRING"),},["="]="BRACKET2",[F] = push_token(nil),},
-	BRACKET2 = {["["] = {[F]=consume_block("STRING"),},["="]="BRACKET2",},
-	COMMENT  = {
+	BRACKET = {
+		["["] = {[F] = consume_block("STRING", "START", 0)},
+		["="] = "BRACKET2",
+		[F] = push_token(nil) --[[Normal brackets - for table index]]},
+	BRACKET2 = {["["] = {[F] = consume_block("STRING")}, ["="] = "BRACKET2"},
+	COMMENT = {
 		["["] = {
-			["="]="CRACKET",
-			["["]={[F]=consume_block("COMMENT","START",4)},
-			[except("\r","\n","=","[")] = "COMMENT2",
+			["="] = "CRACKET",
+			["["] = {[F] = consume_block("COMMENT", "START", 4)},
+			[except("\r", "\n", "=", "[")] = "COMMENT2",
 			[F] = push_token("COMMENT")},
-		[except("\r","\n","[")] = "COMMENT2",
+		[except("\r", "\n", "[")] = "COMMENT2",
 		[F] = push_token("COMMENT")},
 	COMMENT2 = {
-		[except("\r","\n")] = "COMMENT2",
+		[except("\r", "\n")] = "COMMENT2",
 		[F] = push_token("COMMENT")},
-	CRACKET  = {["["] = {[F]=consume_block("COMMENT","START",4),},["="]="CRACKET",[ANY] = "COMMENT2",},
+	CRACKET  = {["["] = {[F] = consume_block("COMMENT", "START", 4)}, ["="] = "CRACKET", [ANY] = "COMMENT2"},
 }
 
 ---
@@ -237,19 +245,21 @@ states = {
 function Lib:Tokenize(str, cb)
 	local st, ret, len, cb = states[START], {}, #str, cb or default_transform;
 	local stack, pos, ln = {}, 1, 1;
-	while pos <= len+1 do
-		local ch = str:sub(pos,pos);      -- get next character. No way around this.
+	while pos <= (len + 1) do
+		local ch = str:sub(pos, pos);     -- get next character. No way around this.
 		local newst = st[ch] or st[ANY];  -- try transition to new state using ch
-		stack[#stack+1] = ch;
+		stack[#stack + 1] = ch;
 		if not newst and st[F] then
-			newst, stack, pos, ln = st[F](states,stack,str,ret,cb,pos,ln);
-			newst = newst[ch] or newst[ANY] or states[ERROR]; 
+			newst, stack, pos, ln = st[F](states, stack, str, ret, cb, pos, ln);
+			-- next state, new pos, new line number
+			--newst = newst[ch] or newst[ANY] or states[ERROR];
+		else
+			pos = pos + 1;
 		end
 		st = newst or states[ERROR];
-		pos = pos + 1;
 	end
 	if #stack > 1 then
-		states[ERROR][F](states,stack,str,ret,cb,pos,ln);
+		states[ERROR][F](states, stack, str, ret, cb, pos, ln);
 	end
 	return ret;
 end
@@ -272,8 +282,7 @@ local function prepare_tree(states, state)
 	end
 	for i=1, #queue do
 		local k = queue[i]
-		state[k] = nil, k(state, state[k]); -- Wow, this was evil of me. Note state[k] is
-		                                    -- only nil'd after assignment
+		state[k] = nil, k(state, state[k]); -- state[k] is only nil'd after assignment
 	end
 end
 prepare_tree(states, states)
