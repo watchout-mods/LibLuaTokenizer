3d81031bd1fe8d6ca0b5511b7764506ee03a659b
watchout-mods <76425756+watchout-mods@users.noreply.github.com>
2021-04-05 14:14:32 +0200

    Reorganise test files

---
+++ examples/Profile.lua
@@ -0,0 +1,40 @@
+local d = os.clock;
+local io = require("io");
+local N = 100;
+
+local str = {};
+for line in io.lines() do
+	str[#str+1] = line;
+end
+str = table.concat(str, "\n");
+
+local cb, t = function() end;
+
+do
+	t = d();
+	dofile("../LuaTokenizer-1.0.lua");
+	print(("Load time: %.3f ms"):format((d()-t)*1000))
+	t = d();
+	for i=1, N do
+		local x = LuaTokenizer:Tokenize(str, cb)
+	end
+	t = d()-t;
+	print(("    Exec time: %.3f s"):format(t))
+	print(("    Avg /it:   %.3f ms"):format(t*1000/N, "ms"))
+	print( "-----------------------")
+end
+
+do
+	t = d();
+	dofile("../LuaTokenizer-1.0-old.lua");
+	print("Old version")
+	print(("Load time: %.3f ms"):format((d()-t)*1000))
+	t = d();
+	for i=1, N do
+		local x = LuaTokenizer:Tokenize(str, cb)
+	end
+	t = d()-t;
+	print(("    Exec time: %.3f s"):format(t))
+	print(("    Avg /it:   %.3f ms"):format(t*1000/N, "ms"))
+	print( "-----------------------")
+end
